3dc789ea616fbd6464513ecf850b898f5699a2a6
olangomark <olangomark@gmail.com>
2023-03-14 17:35:49 +0800

    Initial commit

--- js/now-ui-kit.js
+++ js/now-ui-kit.js
@@ -41,7 +41,7 @@ $(document).ready(function() {
     nowuiKit.initNavbarImage();
 
     $navbar = $('.navbar[color-on-scroll]');
-    scroll_distance = $navbar.attr('color-on-scroll') || 500;
+    scroll_distance = $navbar.attr('color-on-scroll') || 0;
 
     // Check if we have the class "navbar-color-on-scroll" then add the function to remove the class "navbar-transparent" so it will transform to a plain color.
 
